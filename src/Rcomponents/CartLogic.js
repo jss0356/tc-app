f8483c9f9ef1453f28b5c52dc3a0c4771c27d26b
bal0155 <81335657+bal0155@users.noreply.github.com>
2023-08-31 10:25:53 -0500

    added logic for the cart functionality

---
+++ src/Rcomponents/CartLogic.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+function CartLogic({ cart }) {
+  const calculateTotalPrice = () => {
+    return cart.reduce((total, card) => {
+      const cardPrice = card?.tcgplayer?.prices?.holofoil?.market || 0;
+      return total + cardPrice;
+    }, 0);
+  };
+
+  return (
+    <div className="container mt-5">
+      <h2 className="mb-4">Cart</h2>
+      {cart.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <div>
+          <ul className="list-group">
+            {cart.map((card) => (
+              <li key={card.id} className="list-group-item mb-3">
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex align-items-center">
+                    <img
+                      src={card.images.small}
+                      alt={card.name}
+                      className="card-image me-3"
+                    />
+                    <div>
+                      <h5 className="mb-0">{card.name}</h5>
+                      <p className="mb-0">
+                        ${card?.tcgplayer?.prices?.holofoil?.market}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className="flex fixed-bottom bg-light p-3 mr-3 text-center">
+            <p className="h5">Total Price: ${calculateTotalPrice()}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default CartLogic;
