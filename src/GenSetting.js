34ed95a9ffc1a8f812b492b93e6713ae040de9ab
aaronddaron <113373115+aaronddaron@users.noreply.github.com>
2023-09-05 11:57:52 -0500

    Input checks for general and payment settings

--- src/GenSetting.js
+++ src/GenSetting.js
@@ -9,26 +9,27 @@ const GenSettings = () => {
 
     return (
         <div id="setting-options" className="ps-3 d-flex flex-column gap-3 h-100 w-50 border border-dark">
-            <label className="text-center">General Settings</label>
+            <form action="">
+                <label className="text-center">General Settings</label>
 
-            <div id="form-input-account-email d-flex">
-            
-                <label htmlFor="account-email" className="d-inline pe-2">Account Email:</label>
-                <input type="text" value={accountEmail} onChange={(e) => setAccountEmail(e.target.value)} className="w-75 form-control d-inline" />
-            
-            </div>            
-            
-            <div className="d-flex flex-row w-100">
-                <label className="form-label d-inline pe-2" htmlFor="flexSwitchCheckDefault" >Recieve Email Notifications: </label>
+                <div id="form-input-account-email d-flex">
+                
+                    <label htmlFor="account-email" className="d-inline pe-2">Account Email:</label>
+                    <input type="email" placeholder="example@gmail.com"value={accountEmail} onChange={(e) => setAccountEmail(e.target.value)} className="w-75 form-control d-inline" />
+                
+                </div>            
+                
+                <div className="d-flex flex-row w-100">
+                    <label className="form-label d-inline pe-2" htmlFor="flexSwitchCheckDefault" >Recieve Email Notifications: </label>
 
-                <div className="form-check form-switch">
+                    <div className="form-check form-switch">
 
-                    <input className="form-check-input d-inline" type="checkbox" id="flexSwitchCheckDefault" checked={receiveEmailNotifications} onChange={() => setRecieveEmailNotifications(!receiveEmailNotifications)}/>
+                        <input className="form-check-input d-inline" type="checkbox" id="flexSwitchCheckDefault" checked={receiveEmailNotifications} onChange={() => setRecieveEmailNotifications(!receiveEmailNotifications)}/>
 
+                    </div>
                 </div>
-            </div>
-
-            
+                <input type="submit"/>
+            </form>
 
         </div>
     )
--- src/PaySettings.js
+++ src/PaySettings.js
@@ -40,60 +40,62 @@ const PaySettings = () => {
 
             {(selectedPaymentMethod !== "Paypal" && selectedPaymentMethod !== "") && (
             <>
-                <div id="form-input-credit-card-number d-flex">
+                <form>
+                    <div id="form-input-credit-card-number d-flex">
 
-                    <h2 className='text-center'>Card Information:</h2>
-                
-                    <label htmlFor="account-email" className="d-inline pe-2">Credit Card Number:</label>
-                    <input type="text" className="w-50 form-control d-inline" value={creditCardNumber} onChange={(e) => setCreditCardNumber(e.target.value)} />
-                </div>            
-                <div id="form-input-credit-card-number d-flex">
+                        <h2 className='text-center'>Card Information:</h2>
+                    
+                        <label htmlFor="account-email" className="d-inline pe-2">Credit Card Number:</label>
+                        <input type="tel" inputmode="numeric" pattern="[0-9\s]{13,19}" maxlength="19" placeholder="xxxx xxxx xxxx xxxx" className="w-50 form-control d-inline" value={creditCardNumber} onChange={(e) => setCreditCardNumber(e.target.value)} />
+                    </div>            
+                    <div id="form-input-credit-card-number d-flex">
 
-                    <label htmlFor="account-email" className="d-inline pe-2">Expiration Date:</label>
-                    <input type="text" className="w-50 form-control d-inline" value={expirationDate} onChange={(e) => setExpirationDate(e.target.value)} />
-                </div>            
-                <div id="form-input-credit-card-number d-flex">
-                    <label htmlFor="account-email" className="d-inline pe-2">CVV:</label>
-                    <input type="text" className="w-50 form-control d-inline" value={cvv} onChange={(e) => setCVV(e.target.value)}/>
-                </div>            
+                        <label htmlFor="account-email" className="d-inline pe-2">Expiration Date:</label>
+                        <input type="text" patter="\d{4}" placeholder="MMYY" className="w-50 form-control d-inline" value={expirationDate} onChange={(e) => setExpirationDate(e.target.value)} />
+                    </div>            
+                    <div id="form-input-credit-card-number d-flex">
+                        <label htmlFor="account-email" className="d-inline pe-2">CVV:</label>
+                        <input type="text" pattern="\d{3}" placeholder="###" className="w-50 form-control d-inline" value={cvv} onChange={(e) => setCVV(e.target.value)}/>
+                    </div>            
 
-                <h2 className='text-center'>Billing Address:</h2>
+                    <h2 className='text-center'>Billing Address:</h2>
 
-                <div id="form-input-credit-card-number d-flex">
-                    <label htmlFor="account-email" className="d-inline pe-2">Country/Region:</label>
-                    <input type="text" className="w-50 form-control d-inline" value={countryOrRegion} onChange={(e) => setCountryOrRegion(e.target.value)}/>
-                </div>            
+                    <div id="form-input-credit-card-number d-flex">
+                        <label htmlFor="account-email" className="d-inline pe-2">Country/Region:</label>
+                        <input type="text" pattern="[A-Za-z]+" placeholder="Country or Region" className="w-50 form-control d-inline" value={countryOrRegion} onChange={(e) => setCountryOrRegion(e.target.value)}/>
+                    </div>            
 
-                <div id="form-input-credit-card-number d-flex">
-                    <label htmlFor="account-email" className="d-inline pe-2">First Name:</label>
-                    <input type="text" className="w-50 form-control d-inline" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
-                </div>            
+                    <div id="form-input-credit-card-number d-flex">
+                        <label htmlFor="account-email" className="d-inline pe-2">First Name:</label>
+                        <input type="text" pattern="[A-Za-z]+" placeholder="First" className="w-50 form-control d-inline" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+                    </div>            
 
-                <div id="form-input-credit-card-number d-flex">
-                    <label htmlFor="account-email" className="d-inline pe-2">Last Name:</label>
-                    <input type="text" className="w-50 form-control d-inline" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
-                </div>            
+                    <div id="form-input-credit-card-number d-flex">
+                        <label htmlFor="account-email" className="d-inline pe-2">Last Name:</label>
+                        <input type="text" pattern="[A-Za-z]+" placeholder="Last" className="w-50 form-control d-inline" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+                    </div>            
 
-                <div id="form-input-credit-card-number d-flex">
-                    <label htmlFor="account-email" className="d-inline pe-2">Street Address:</label>
-                    <input type="text" className="w-50 form-control d-inline" value={streetAddress} onChange={(e) => setStreetAddress(e.target.value)}/>
-                </div>            
+                    <div id="form-input-credit-card-number d-flex">
+                        <label htmlFor="account-email" className="d-inline pe-2">Street Address:</label>
+                        <input type="text" pattern="^(\d{1,}) [a-zA-Z0-9\s]+" className="w-50 form-control d-inline" value={streetAddress} onChange={(e) => setStreetAddress(e.target.value)}/>
+                    </div>            
 
-                <div id="form-input-credit-card-number d-flex">
-                    <label htmlFor="account-email" className="d-inline pe-2">Apt/Suite/Building (Optional):</label>
-                    <input type="text" className="w-50 form-control d-inline" value={aptOrSuiteOrBuilding} onChange={(e) => setAptOrSuiteOrBuilding(e.target.value)}/>
-                </div>            
+                    <div id="form-input-credit-card-number d-flex">
+                        <label htmlFor="account-email" className="d-inline pe-2">Apt/Suite/Building (Optional):</label>
+                        <input type="text" className="w-50 form-control d-inline" value={aptOrSuiteOrBuilding} onChange={(e) => setAptOrSuiteOrBuilding(e.target.value)}/>
+                    </div>            
 
-                <div id="form-input-credit-card-number d-flex">
-                    <label htmlFor="account-email" className="d-inline pe-2">Zip Code:</label>
-                    <input type="text" className="w-50 form-control d-inline" value={zipCode} onChange={(e) => setZipCode(e.target.value)}/>
-                </div>            
+                    <div id="form-input-credit-card-number d-flex">
+                        <label htmlFor="account-email" className="d-inline pe-2">Zip Code:</label>
+                        <input type="text" pattern="\d{5}" placeholder="#####" className="w-50 form-control d-inline" value={zipCode} onChange={(e) => setZipCode(e.target.value)}/>
+                    </div>            
 
-                <div id="form-input-credit-card-number d-flex">
-                    <label htmlFor="account-email" className="d-inline pe-2">City, State:</label>
-                    <input type="text" className="w-50 form-control d-inline" value={cityOrState} onChange={(e) => setCityOrState(e.target.value)}/>
-                </div>            
-
+                    <div id="form-input-credit-card-number d-flex">
+                        <label htmlFor="account-email" className="d-inline pe-2">City, State:</label>
+                        <input type="text" pattern="[A-Za-z]+" placeholder="City or State" className="w-50 form-control d-inline" value={cityOrState} onChange={(e) => setCityOrState(e.target.value)}/>
+                    </div>   
+                    <input type="submit"/>         
+                </form>
 
             </>
             )}
