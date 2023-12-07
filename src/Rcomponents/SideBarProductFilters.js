bd2f3777481d36efdd18e4cee5e4987da1ef695d
bal0155 <81335657+bal0155@users.noreply.github.com>
2023-12-07 00:40:37 -0600

    added message to availability and condition filters

--- src/Rcomponents/SideBarProductFilters.js
+++ src/Rcomponents/SideBarProductFilters.js
@@ -49,6 +49,21 @@ const SideBarProductFilters = ({
   const switchBetweenFilters = () => {
     setMarketPriceFilters(!marketPriceFilters);
   };
+  const [messageForCondition, setMessageForCondition] = useState(false);
+  const hoverOnCondition = () => {
+    setMessageForCondition(true);
+  };
+  const hoverOffCondition = () => {
+    setMessageForCondition(false);
+  };
+  const [messageForAvailability, setMessageForAvailability] = useState(false);
+  const hoverOnAvailability = () => {
+    setMessageForAvailability(true);
+  };
+  const hoverOffAvailability = () => {
+    setMessageForAvailability(false);
+  };
+
   return (
     <div
       className="sidebar"
@@ -154,26 +169,59 @@ const SideBarProductFilters = ({
       ) : (
         <div>
           {/* <h4 className="mt-2">Filters For User Listings</h4> */}
-          <Form.Label className="mt-3">Availability</Form.Label>
-          <Form.Group className="mb-3">
-            <Form.Check type="checkbox" label="Available" />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Check type="checkbox" label="Not Available" />
-          </Form.Group>
-          <Form.Label className="mt-3">Condition</Form.Label>
-          <Form.Group className="mb-3">
-            <Form.Check type="checkbox" label="Near Mint" />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Check type="checkbox" label="Lighly Played" />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Check type="checkbox" label="Moderately Played" />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Check type="checkbox" label="Heavly Played" />
-          </Form.Group>
+          <div
+            onMouseEnter={hoverOnAvailability}
+            onMouseLeave={hoverOffAvailability}
+          >
+            <Form.Label className="mt-3">Availability</Form.Label>
+            <Form.Group className="mb-3">
+              <Form.Check type="checkbox" label="Available" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Check type="checkbox" label="Not Available" />
+            </Form.Group>
+          </div>
+          {messageForAvailability && (
+            <div
+              className="text-center justify-content-center align-items-center"
+              style={{
+                backgroundColor: "rgba(200, 0, 0, 1)",
+                color: "white",
+                borderRadius: "2px",
+                zIndex: 1,
+              }}
+            >
+              <p>Availability filter currently unavailable</p>
+            </div>
+          )}
+          <div onMouseEnter={hoverOnCondition} onMouseLeave={hoverOffCondition}>
+            <Form.Label className="mt-3">Condition</Form.Label>
+            <Form.Group className="mb-3">
+              <Form.Check type="checkbox" label="Near Mint" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Check type="checkbox" label="Lighly Played" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Check type="checkbox" label="Moderately Played" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Check type="checkbox" label="Heavly Played" />
+            </Form.Group>
+          </div>
+          {messageForCondition && (
+            <div
+              className="text-center justify-content-center align-items-center"
+              style={{
+                backgroundColor: "rgba(200, 0, 0, 1)",
+                color: "white",
+                borderRadius: "2px",
+                zIndex: 1,
+              }}
+            >
+              <p>condition filter currently unavailable</p>
+            </div>
+          )}
           <Form.Group className="mb-2">
             <Form.Label>Price Range</Form.Label>
             <div className="d-flex align-items-center">
